be569ca924df7b822ba40ff1b541ba7d8a7bef23
JULIEN BOUYOUD <julien-externe.bouyoud@edf.fr>
2017-03-03 11:25:10 +0100

    feat(config): coprocessors policy withe list

--- src/main/java/fr/poc/hbase/coprocessor/policy/config/PoliciesConfigurer.java
+++ src/main/java/fr/poc/hbase/coprocessor/policy/config/PoliciesConfigurer.java
@@ -1,11 +1,13 @@
 package fr.poc.hbase.coprocessor.policy.config;
 
 import fr.poc.hbase.coprocessor.policy.Policy;
+import fr.poc.hbase.coprocessor.policy.adapter.CoprocessorPolicyAdapter;
 import fr.poc.hbase.coprocessor.policy.impl.*;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.Coprocessor;
 import org.apache.hadoop.metrics2.lib.DefaultMetricsSystem;
 
 import java.util.Arrays;
@@ -20,10 +22,52 @@ import java.util.concurrent.TimeUnit;
 public class PoliciesConfigurer {
 
 	/**
+	 * Coprocessors policies white list
+	 */
+	public static final String COPROCESSOR_POLICY_WITHE_LIST_CONFIGURATION_NAME = "hbase.coprocessors.policy.white-list";
+	/**
+	 * Default white listed classes
+	 */
+	public static final String COPROCESSOR_POLICY_WITHE_LIST_DEFAULT =
+		"org.apache.hadoop.hbase.security.access.SecureBulkLoadEndpoint,"+
+		"org.apache.hadoop.hbase.coprocessor.MultiRowMutationEndpoint,"+
+		"org.apache.hadoop.hbase.backup.master.BackupController";
+
+	/**
 	 * Hbase configuration where policies are extracted in
 	 */
 	@NonNull
 	private final Configuration configuration;
+
+	/**
+	 * Indicates whenever the given coprocessor should be transformed with policies or not
+	 *
+	 * @param priority    coprocessor priority
+	 * @param coprocessor coprocessor
+	 * @return true if policies should be applied, false else
+	 */
+	public boolean needApplyPolicies(int priority, @NonNull Coprocessor coprocessor) {
+		// Do not apply policies when they are already sets
+		if (coprocessor instanceof CoprocessorPolicyAdapter) {
+			return false;
+		}
+		// Do not apply policies on withe list classes
+		if (getCoprocessorsWitheListClasses().stream()
+				.anyMatch(c -> c.equals(coprocessor.getClass().getName()))) {
+			return false;
+		}
+		// Else apply
+		return true;
+	}
+
+	/**
+	 * Parse with listed classes from configuration
+	 *
+	 * @return list of withe listed classes names
+	 */
+	private List<String> getCoprocessorsWitheListClasses() {
+		return Arrays.asList(configuration.get(COPROCESSOR_POLICY_WITHE_LIST_CONFIGURATION_NAME, COPROCESSOR_POLICY_WITHE_LIST_DEFAULT).split(","));
+	}
 
 	/**
 	 * Build policies from the current configuration
@@ -37,8 +81,9 @@ public class PoliciesConfigurer {
 				new LoggingPolicy(),
 				new MetricsPolicy(DefaultMetricsSystem.instance(), "Coprocessors"),
 				new LimitRetryPolicy(2, new LimitRetryPolicy.RollingInMemoryCache(10, TimeUnit.MINUTES)),
-				//new MaxMemoryPolicy(1024 * 1024 * 10L, 500),
 				new NoBypassOrCompletePolicy()
 		);
 	}
+
+
 }
--- src/main/java/fr/poc/hbase/coprocessor/policy/util/PoliciesHelper.java
+++ src/main/java/fr/poc/hbase/coprocessor/policy/util/PoliciesHelper.java
@@ -1,8 +1,6 @@
 package fr.poc.hbase.coprocessor.policy.util;
 
-import fr.poc.hbase.coprocessor.policy.Policy;
 import fr.poc.hbase.coprocessor.policy.PolicyInvocationHandler;
-import fr.poc.hbase.coprocessor.policy.adapter.CoprocessorPolicyAdapter;
 import fr.poc.hbase.coprocessor.policy.config.PoliciesConfigurer;
 import lombok.NonNull;
 import lombok.experimental.UtilityClass;
@@ -21,6 +19,7 @@ import java.util.List;
 @UtilityClass
 public class PoliciesHelper {
 
+
 	/**
 	 * Adapt a coprocessor that have at least a #PRIORITY_USER priority with polices
 	 * witch are build from hbase configuration
@@ -30,40 +29,25 @@ public class PoliciesHelper {
 	 * @param configuration Hbase configuration where policies configuration will be fetched in
 	 * @return apadated coprocessor with policies
 	 */
+	@SuppressWarnings("unchecked")
 	public static Coprocessor withPolicies(@NonNull Coprocessor coprocessor, int priority,
 										   @NonNull Configuration configuration) {
-		// Do not apply policies when they are already sets
-		if (coprocessor instanceof CoprocessorPolicyAdapter) {
-			return coprocessor;
-		}
-		// If coprocessor is at system level, check if it's an hbase ones
-		if (priority <= org.apache.hadoop.hbase.Coprocessor.PRIORITY_SYSTEM
-				&& coprocessor.getClass().getName().startsWith(Coprocessor.class.getPackage().getName())) {
-			// DO not modify system priority coprocessors
-			return coprocessor;
-		}
-		// build policies from configuration
-		return withPolicies(coprocessor, new PoliciesConfigurer(configuration).getPolicies());
-	}
 
-	/**
-	 * Adapt a coprocessor with given polices
-	 *
-	 * @param coprocessor Coprocessor where policies should be applied
-	 * @param policies    Policies to apply
-	 * @param <T>         Coprocessor type
-	 * @return apadated coprocessor with policies
-	 */
-	@SuppressWarnings("unchecked")
-	public static <T extends Coprocessor> T withPolicies(@NonNull T coprocessor, @NonNull List<Policy> policies) {
+		PoliciesConfigurer configurer = new PoliciesConfigurer(configuration);
+		// If the current configuration allows this coprocessor
+		if (!configurer.needApplyPolicies(priority, coprocessor)) {
+			// DO not modify others coprocessors
+			return coprocessor;
+		}
+		// Fetch interfaces
 		List interfaces = ClassUtils.getAllInterfaces(coprocessor.getClass());
 		Class[] ifaces = (Class[]) interfaces.toArray(new Class[interfaces.size()]);
 
 		LOGGER.info("Create a PolicyVerifier on Coprocessor : [{}]", coprocessor);
-		return (T) Proxy.newProxyInstance(
+		// Create a proxy for the current coprocessor based on each interfaces
+		return (Coprocessor) Proxy.newProxyInstance(
 				coprocessor.getClass().getClassLoader(), ifaces,
-				new PolicyInvocationHandler<>(coprocessor, ifaces, policies));
+				new PolicyInvocationHandler<>(coprocessor, ifaces, configurer.getPolicies()));
 	}
 
 }
-
